7c719c308a758e8dea90705a3f3bd253a5e7cba8
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-16 18:52:13 +0100

    testing

--- frontend/src/components/CreateRoom.jsx
+++ frontend/src/components/CreateRoom.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 function CreateRoom() {
-  return <h1>Create Room Page</h1>;
+  useEffect(() => {
+    let code = "";
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+      const urlParams = new URLSearchParams(queryString);
+      code = urlParams.get("code");
+    }
+    console.log(code);
+  }, []);
+
+  return (
+    <div>
+      <h1>Create Room Page</h1>
+
+      <Link to="/">
+        <button>Home Page</button>
+      </Link>
+    </div>
+  );
 }
 
 export default CreateRoom;
--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function JoinRoom() {
-  return <h1>Join Room Page</h1>;
+  return (
+    <div>
+      <h1>Join Room Page</h1>
+
+      <Link to="/">
+        <button>Home Page</button>
+      </Link>
+    </div>
+  );
 }
 
 export default JoinRoom;
