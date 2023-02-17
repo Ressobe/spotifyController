1ed1070faa49f2e92dce26acb6633f15b1503108
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-17 09:23:58 +0100

    Add custom serializer for token

---
+++ backend/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["id"] = user.id
+        token["client_id"] = user.client_id
+        token["client_secret"] = user.client_secret
+        return token
