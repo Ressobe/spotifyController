9ddedcb37fb8992e9c3306aed9e20a2541fef4b3
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-18 21:12:42 +0100

    Add url and view for registration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+
+perimissions = (
+  "user-read-private",
+  "user-read-email",
+  "user-modify-playback-state",
+  "user-read-playback-position",
+  "user-library-read",
+  "streaming",
+  "user-read-playback-state",
+  "user-read-recently-played",
+  "playlist-read-private",
+
+)
+
+ROUTES = ["token/", "token/refresh", "register/"]
+
+class GetRoutes(APIView):
+    def get(self, request):
+        return Response(ROUTES)
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
