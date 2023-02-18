27a3925e89223122ca2f3da7f469d0bb22fe226b
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-18 21:28:22 +0100

    Add auth provider for all routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,18 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "../context/authContext";
 import AuthenticateUser from "./AuthenticateUser";
 import CreateRoom from "./CreateRoom";
 import JoinRoom from "./JoinRoom";
 import HomePage from "./HomePage";
+import LoginUser from "./LoginUser";
+import RegisterUser from "./RegisterUser";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/authorize" element={<AuthenticateUser />} />
-        <Route path="/create" element={<CreateRoom />} />
-        <Route path="/join" element={<JoinRoom />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/authorize" element={<AuthenticateUser />} />
+          <Route path="/create" element={<CreateRoom />} />
+          <Route path="/join" element={<JoinRoom />} />
+          <Route path="/login" element={<LoginUser />} />
+          <Route path="/register" element={<RegisterUser />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
