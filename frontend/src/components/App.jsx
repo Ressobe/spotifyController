8e97c58c159b05a0191ce12b96832c1541b18e13
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-22 11:13:30 +0100

    Wrap room routes and add route for notfoundpage

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,24 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
-import CreateRoom from "../pages/CreateRoomPage";
-import JoinRoom from "../pages/JoinRoomPage";
-import HomePage from "../pages/HomePage";
+import CreateRoom from "../pages/CreateRoom";
+import JoinRoom from "../pages/JoinRoom";
+import HomePage from "../pages/Home";
 import LoginUser from "../pages/LoginUser";
 import RegisterUser from "../pages/RegisterUser";
+import NotFound from "../pages/NotFound";
+import RoomNav from "./RoomNav";
+import Room from "../pages/Room";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreateRoom />} />
-          <Route path="/join" element={<JoinRoom />} />
-          <Route path="/login" element={<LoginUser />} />
-          <Route path="/register" element={<RegisterUser />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/room" element={<RoomNav />}>
+          <Route path="create" element={<CreateRoom />} />
+          <Route path="join" element={<JoinRoom />} />
+          <Route path=":id" element={<Room />} />
+        </Route>
+
+        <Route path="/login" element={<LoginUser />} />
+        <Route path="/register" element={<RegisterUser />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
