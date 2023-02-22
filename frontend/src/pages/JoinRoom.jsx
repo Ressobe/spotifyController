7bb1c45e84f519df38d91b3cd2ccd0ae707f50a0
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-22 11:04:03 +0100

    Rename file

--- frontend/src/pages/JoinRoom.jsx
+++ frontend/src/pages/JoinRoom.jsx
@@ -1,13 +1,7 @@
-import { Link } from "react-router-dom";
-
 function JoinRoom() {
   return (
     <div>
       <h1>Join Room Page</h1>
-
-      <Link to="/">
-        <button>Home Page</button>
-      </Link>
     </div>
   );
 }
