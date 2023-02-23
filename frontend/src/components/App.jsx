02a7263a4df64e4a2007e48444a8130875e5c2cc
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-23 09:09:01 +0100

    Add private routes and navbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
+import PrivateRoutes from "../utils/PrivateRoutes";
+import PrivateRoutesLogged from "../utils/PrivateRoutesLogged";
 import CreateRoom from "../pages/CreateRoom";
 import JoinRoom from "../pages/JoinRoom";
 import HomePage from "../pages/Home";
@@ -8,21 +10,30 @@ import RegisterUser from "../pages/RegisterUser";
 import NotFound from "../pages/NotFound";
 import RoomNav from "./RoomNav";
 import Room from "../pages/Room";
+import Profile from "../pages/Profile";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <AuthProvider>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
 
-        <Route path="/room" element={<RoomNav />}>
-          <Route path="create" element={<CreateRoom />} />
-          <Route path="join" element={<JoinRoom />} />
-          <Route path=":id" element={<Room />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/room" element={<RoomNav />}>
+            <Route path="create" element={<CreateRoom />} />
+            <Route path="join" element={<JoinRoom />} />
+            <Route path=":id" element={<Room />} />
+          </Route>
+          <Route path="/profile" element={<Profile />} />
         </Route>
 
-        <Route path="/login" element={<LoginUser />} />
-        <Route path="/register" element={<RegisterUser />} />
+        <Route element={<PrivateRoutesLogged />}>
+          <Route path="/login" element={<LoginUser />} />
+          <Route path="/register" element={<RegisterUser />} />
+        </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
