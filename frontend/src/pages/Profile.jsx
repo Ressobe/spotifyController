a18b3327bc73eb9b138f56af922d769e0ac85c32
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-23 09:13:05 +0100

    Initial commit

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+  return (
+    <div className="bg-stone-100 flex justify-center items-center h-screen">
+      <div className="w-full max-w-xs text-center">
+        <h1>Profile Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
