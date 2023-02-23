9001db3b9925066b004d4b8528f76e5d76a353dd
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-23 09:11:03 +0100

    Rename file

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className="bg-stone-100 flex justify-center items-center h-screen">
+      <div className="w-full max-w-xs text-center">
+        <h1>Home Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
