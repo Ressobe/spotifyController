bc0f3d8fb89541432196d3519f8502cb61aa70cb
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-27 20:20:23 +0100

    Add function to register user

--- frontend/src/adapters/RegisterUser.js
+++ frontend/src/adapters/RegisterUser.js
@@ -1,3 +1,21 @@
-import SERVER from "./ServerInfo";
+import { REGISTER_URL } from "./ServerInfo";
 
-const RegisterUser = () => {};
+const registerUser = async (username, email, password) => {
+  let data = await fetch(REGISTER_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return data;
+};
+
+export default registerUser;
