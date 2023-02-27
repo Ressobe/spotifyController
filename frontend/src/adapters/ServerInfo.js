fdcd188b4936fb74a6f803c65d8177804e1d39a7
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-02-27 20:20:49 +0100

    Add more info about backend server

--- frontend/src/adapters/ServerInfo.js
+++ frontend/src/adapters/ServerInfo.js
@@ -1,3 +1,14 @@
-const SERVER = "http://127.0.0.1:8000";
+const SERVER = "http://127.0.0.1:8000/api/";
+
+const FETCH_TOKEN_URL = `${SERVER}token/`;
+const FETCH_TOKEN_REFRESH_URL = `${SERVER}token/refresh/`;
+const REGISTER_URL = `${SERVER}register/`;
+const UPDATE_CLIENT_ID_SECRET = `${SERVER}client_id_secret/`;
 
 export default SERVER;
+export {
+  FETCH_TOKEN_URL,
+  FETCH_TOKEN_REFRESH_URL,
+  REGISTER_URL,
+  UPDATE_CLIENT_ID_SECRET,
+};
