8be7fcd27648fe76d24ca3677879d5754d2da454
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-03-08 10:12:17 +0100

    Initial commit

---
+++ frontend/src/components/PageWrapper.jsx
@@ -0,0 +1,9 @@
+function PageWrapper() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-xs text-center"></div>
+    </div>
+  );
+}
+
+export default PageWrapper;
