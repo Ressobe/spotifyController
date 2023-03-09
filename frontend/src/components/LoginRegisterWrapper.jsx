0a4ee3be5b18ca0299e1e6ea1762735f3ffbb739
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-03-09 16:21:56 +0100

    Hard reset

--- frontend/src/components/LoginRegisterWrapper.jsx
+++
@@ -1,9 +0,0 @@
-function LoginRegisterWrapper({ children }) {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-xs">{children}</div>
-    </div>
-  );
-}
-
-export default LoginRegisterWrapper;
--- frontend/src/components/PageWrapper.jsx
+++
@@ -1,9 +0,0 @@
-function PageWrapper({ children }) {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-xs text-center">{children}</div>
-    </div>
-  );
-}
-
-export default PageWrapper;
